2370d4c0d38c275979773314a008eb3094f736b9
Aditya Mankar <adityamankar022@gmail.com>
2026-05-18 20:15:56 +0530

    non returns function

---
+++ non returns function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//non return
+
+void sum(int a,int b){
+  cout<<a+b;
+}
+
+
+
+int main()
+{
+int num1,num2;
+cout<<"Enter First number";
+cin>>num1;
+cout<<"Enter second number";
+cin>>num2;
+sum(num1,num2);
+}
+
