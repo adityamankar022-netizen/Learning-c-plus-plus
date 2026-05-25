eb392b15a555cfa862c5283141a86d0dc3d7bc3f
Aditya Mankar <adityamankar022@gmail.com>
2026-05-25 17:30:03 +0530

    pattern alpha

---
+++ pattern alpha.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+/*A
+AB
+ABC
+ABCD
+ABCDE*/
+
+
+   /* for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+        if(col<=row){
+            cout<<char(64+col);
+        }
+        else{
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+
+}
+*/
+
+
+}
