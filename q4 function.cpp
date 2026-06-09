ae28fc27f39223542006a048dd28509e4f8bebb8
Aditya Mankar <adityamankar022@gmail.com>
2026-06-09 14:01:32 +0530

    Add files via upload

---
+++ q4 function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void factorial(int n)
+{
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    cout << "Factorial = " << fact;
+}
+
+int main()
+{
+    int num;
+
+    cout << "Enter number: ";
+    cin >> num;
+
+    factorial(num);
+
+}
+
