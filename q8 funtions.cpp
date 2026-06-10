4f47a5dff32e1c8fb6ceedcbf705138e0415ed23
Aditya Mankar <adityamankar022@gmail.com>
2026-06-10 13:21:46 +0530

    Add files via upload

---
+++ q8 funtions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void cur(int radius)
+{
+
+cout<<"Circumference of a Circle ="<<2*3.14*radius;
+
+}
+
+
+//Q.8 Write a function to find out the given number is prime or not.
+
+passing radius.
+int main()
+{
+cur(5);
+}
