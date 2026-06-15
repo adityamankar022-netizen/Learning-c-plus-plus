86c3aa8fb63a273c0b5f81bc13022f26be1a5fe8
Aditya Mankar <adityamankar022@gmail.com>
2026-06-15 18:54:18 +0530

    ,

---
+++ q6 funtions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void inc(int salary)
+{
+cout<<"salary after increment =";
+cout<<salary = salary*10/100;
+}
+
+
+
+
+
+//Q.6 Write a function to take input as salary & return the salary incremented by 10 %
+
+
+int main()
+{
+    int salary;
+cout<<"Enter your salary: ";
+cin>>salary;
+inc(salary);
+
+
+
+
+}
